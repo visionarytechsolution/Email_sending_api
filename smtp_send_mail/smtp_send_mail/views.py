ae62f73e081658187cb6e48f517d0a99fd7b1e87
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2023-08-09 22:34:04 +0600

    file upload

--- smtp_send_mail/smtp_send_mail/views.py
+++ smtp_send_mail/smtp_send_mail/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 from django.contrib import messages
 def index_page(request):
     if request.method == "POST" :
-        messages.info(request,"File uploaded successfully !!!")
+        try:
+            subject_file = request.FILES['subject_file']
+            # sender_email_conf = request.FILES['sender_email_conf']
+            # rcvr_emails = request.FILES['rcvr_emails']
+            # html_body = request.FILES['html_body']
+            # html_body_content = request.FILES['html_body_content']
+
+            subject_file_data = subject_file.readline()
+            print(subject_file_data)
+            messages.info(request,"File uploaded successfully !!!")
+        except Exception as e:
+            messages.error(request,str(e))
 
     context = {
 
